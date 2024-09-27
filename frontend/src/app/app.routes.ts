330eb9774427f3ae48356ea51673f5aa6d05969c
JaelMartinez <lacarei1221@gmail.com>
2024-09-27 14:53:29 -0600

    pdf

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,29 +1,43 @@
 import { Routes } from '@angular/router';
 
-
 export const routes: Routes = [
   {
     path: 'Admin',
-    loadComponent: () => import('./layouts/main-layout/main-layout.component').then(m => m.MainLayoutComponent),
+    loadComponent: () =>
+      import('./layouts/main-layout/main-layout.component').then(
+        (m) => m.MainLayoutComponent
+      ),
     children: [
       {
         path: 'Registros',
-        loadComponent: () => import('./pages/registros/registros.component').then(m => m.RegistrosComponent)
+        loadComponent: () =>
+          import('./pages/registros/registros.component').then(
+            (m) => m.RegistrosComponent
+          ),
       },
       {
         path: 'Crear',
-        loadComponent: () => import('./pages/create-task-page/create-task-page.component').then(m => m.CreateTaskPageComponent)
+        loadComponent: () =>
+          import('./pages/create-task-page/create-task-page.component').then(
+            (m) => m.CreateTaskPageComponent
+          ),
       },
-      { path: '**', redirectTo: 'Registros' }
-    ]
+      { path: '**', redirectTo: 'Registros' },
+    ],
   },
   {
     path: 'login',
-    loadComponent: () => import('./pages/login-page/login-page.component').then(m => m.LoginPageComponent)
+    loadComponent: () =>
+      import('./pages/login-page/login-page.component').then(
+        (m) => m.LoginPageComponent
+      ),
   },
   {
     path: 'employements',
-    loadComponent: () => import('./layouts/main-layout-employments/main-layout-employments.component').then(m => m.MainLayoutEmploymentsComponent)
+    loadComponent: () =>
+      import(
+        './layouts/main-layout-employments/main-layout-employments.component'
+      ).then((m) => m.MainLayoutEmploymentsComponent),
   },
-  { path: '**', redirectTo: 'login' }
+  { path: '**', redirectTo: 'login' },
 ];
--- frontend/src/app/pages/registros/registros.component.ts
+++ frontend/src/app/pages/registros/registros.component.ts
@@ -1,41 +1,55 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 import { SidebarModule } from 'primeng/sidebar';
-import { TasksService } from '../../services/tasks.service';
 import { Table, TableModule } from 'primeng/table';
 import { TagModule } from 'primeng/tag';
 import { ButtonModule } from 'primeng/button';
 import { RippleModule } from 'primeng/ripple';
-import { data } from './data_test';
-import { TechListResponse, Technitian } from '../../interfaces/techniciansListResponse.interface';
 import { InputIconModule } from 'primeng/inputicon';
 import { IconFieldModule } from 'primeng/iconfield';
 import { InputTextModule } from 'primeng/inputtext';
-import { FormsModule, NgModel } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+import { Technitian } from '../../interfaces/techniciansListResponse.interface';
+import { data } from './data_test';
+import jsPDF from 'jspdf';
+import html2canvas from 'html2canvas';
 
 @Component({
   selector: 'app-registros',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive,SidebarModule, CommonModule,TableModule,TagModule,ButtonModule, RippleModule,InputIconModule,IconFieldModule, InputTextModule,FormsModule],
-  providers: [TasksService],
+  imports: [
+    CommonModule,
+    RouterLink,
+    RouterLinkActive,
+    SidebarModule,
+    TableModule,
+    TagModule,
+    ButtonModule,
+    RippleModule,
+    InputIconModule,
+    IconFieldModule,
+    InputTextModule,
+    FormsModule,
+  ],
   templateUrl: './registros.component.html',
-  styleUrls: ['./registros.component.css']
+  styleUrls: ['./registros.component.css'],
 })
 export class RegistrosComponent implements OnInit {
   @ViewChild('TechsTable') TechsTable!: Table;
-  searchTerm: string = ''; 
-  data: TechListResponse={
-    technitians:[],
-    totalTechs:0
-  }; // Aquí almacenaremos los datos
-  datafields:string[] = ["Id","Trabajador","Numero de cliente","Tareas","Estatus","Fecha de tarea","Puntos obtenidos"];
-  showSideNav: boolean = false;
+  searchTerm: string = '';
+  data = data; // Aquí tienes tus datos
+  totalTechs: number = 0;
+
   ngOnInit(): void {
-    // Datos simulados para prueba
+    // Asigna los datos y calcula el total de técnicos
     this.data = data;
+    this.totalTechs = this.data.technitians.length;
   }
-  getStatusTag(status: string) :"success" | "secondary" | "info" | "warning" | "danger"  | undefined {
+
+  getStatusTag(
+    status: string
+  ): 'success' | 'secondary' | 'info' | 'warning' | 'danger' | undefined {
     switch (status) {
       case 'Completada':
         return 'success';
@@ -49,10 +63,12 @@ export class RegistrosComponent implements OnInit {
         return undefined;
     }
   }
-  getTotalPoints(data: Technitian):number {
+
+  getTotalPoints(data: Technitian): number {
     return data.tasks.reduce((acc: number, task: any) => acc + task.puntos, 0);
   }
-  getTotalBono(data:any) {
+
+  getTotalBono(data: Technitian): number {
     const points = this.getTotalPoints(data);
     if (points <= 80) {
       return 0;
@@ -64,9 +80,54 @@ export class RegistrosComponent implements OnInit {
       return 650;
     }
   }
+
   filterGlobal(event: any) {
     if (this.TechsTable) {
       this.TechsTable.filterGlobal(event.target.value, 'contains');
     }
+  }
+  generatePDF() {
+    // Mostrar todas las filas antes de generar el PDF
+    const originalRows = this.TechsTable.rows;
+    this.TechsTable.rows = this.totalTechs;
+    this.TechsTable.reset();
+
+    setTimeout(() => {
+      const dataElement = document.getElementById('tablesToExport');
+      if (dataElement) {
+        html2canvas(dataElement, {
+          onclone: (clonedDoc) => {
+            // Ocultar elementos no deseados en el documento clonado
+            clonedDoc.querySelectorAll('.elementsToHide').forEach((element) => {
+              (element as HTMLElement).style.display = 'none';
+            });
+          },
+        }).then((canvas) => {
+          const imgWidth = 210;
+          const pageHeight = 295;
+          const imgHeight = (canvas.height * imgWidth) / canvas.width;
+          let heightLeft = imgHeight;
+
+          const pdf = new jsPDF('p', 'mm', 'a4');
+          let position = 0;
+
+          pdf.addImage(canvas, 'PNG', 0, position, imgWidth, imgHeight);
+          heightLeft -= pageHeight;
+
+          while (heightLeft > 0) {
+            position = heightLeft - imgHeight;
+            pdf.addPage();
+            pdf.addImage(canvas, 'PNG', 0, position, imgWidth, imgHeight);
+            heightLeft -= pageHeight;
+          }
+
+          pdf.save('tabla-tecnicos.pdf');
+
+          // Restaurar el número original de filas
+          this.TechsTable.rows = originalRows;
+          this.TechsTable.reset();
+        });
+      }
+    }, 1000); // Ajusta el tiempo según sea necesario
   }
 }
